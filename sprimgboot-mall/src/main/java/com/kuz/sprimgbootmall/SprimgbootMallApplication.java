70034b0b0d077ee10ba5b5393751bf1e8716ac63
kuz <wangdanz210198@gmail.com>
2024-05-26 22:30:11 +0800

    初始化spring boot projrct

---
+++ sprimgboot-mall/src/main/java/com/kuz/sprimgbootmall/SprimgbootMallApplication.java
@@ -0,0 +1,13 @@
+package com.kuz.sprimgbootmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprimgbootMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprimgbootMallApplication.class, args);
+    }
+
+}
---
+++ sprimgboot-mall/src/test/java/com/kuz/sprimgbootmall/SprimgbootMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuz.sprimgbootmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprimgbootMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
